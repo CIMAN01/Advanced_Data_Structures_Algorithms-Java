7a4581bd55c8de1b29267305f22f91fb3d708940
Cameron Imanpour <34729011+CIMAN01@users.noreply.github.com>
2021-08-04 14:18:48 -0700

    Clean up code and add optimization and comments
    Add a second sort method with an optimization. Refactor code and add comments.

--- Sort Algorithms/BubbleSort.java
+++ Sort Algorithms/BubbleSort.java
@@ -1,40 +1,77 @@
-
-
+// main class
 public class BubbleSort {
 
-    public static void main(String[] args) {
-
-        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
-
-        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0;
-             lastUnsortedIndex--) {
-            //
+    // a method that sorts an array by using bubble sort -> best time complexity: O(n) | worst time complexity: O(n^2)
+    public static void sort(int[] array) {
+        // iterate over the entire array
+        for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            // iterate over the unsorted partition of the array
             for (int i = 0; i < lastUnsortedIndex; i++) {
-                //
-                if (intArray[i] > intArray[i + 1]) {
-                    swap(intArray, i, i + 1);
+                // perform a swap as needed
+                if (array[i] > array[i + 1]) {
+                    swap(array, i, i + 1);
                 }
             }
         }
-
-        // print the array
-        for (int i = 0; i < intArray.length; i++) {
-            System.out.println(intArray[i]);
-        }
-
-
     }
 
-    // swap method
+    // a method that sorts an array by using bubble sort -> best time complexity: O(n) | worst time complexity: O(n^2)
+    public static void sort2(int[] array) {
+        // create a boolean condition needed for an optimization (fewer iterations needed)
+        boolean isSorted;
+        // iterate over the entire array
+        for (int i = 0; i < array.length; i++) {
+            // an optimization for when the array is already sorted or partially sorted
+            isSorted = true; // true means we need fewer than n iterations
+            // iterate and compare the second item with the previous item (compare every two items)
+            for (int j = 1; j < array.length-i; j++) {
+                // check if items are out of order and perform a swap if they are
+                if(array[j] < array[j-1]) {
+                    swap(array, j, j-1); // call swap method
+                    isSorted = false; // update condition
+                }
+            }
+            // optimization: if array is already sorted there is no need to iterate over the array one more time
+            if (isSorted) {
+                return;
+            }
+        }
+    }
+
+    // a method that swaps two items based on their indices 
     public static void swap(int[] array, int i, int j) {
-        // base case
+        // if items are equal there is no need to swap
         if (i == j) {
             return;
         }
-        // swap
+        // swap items
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
+    }
+
+    // a method that prints the value of each of the elements of an array
+    public static void print(int[] array) {
+        System.out.println();
+        // iterate over the array and print each element's value
+        for (int j : array) {
+            System.out.print(j + "  ");
+        }
+        System.out.println();
+    }
+
+    // main method
+    public static void main(String[] args) {
+        // create an array with some random values
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+        // call the first method that sorts the array
+        sort(intArray);
+        // print the entire array
+        print(intArray);
+        // call the second method that sorts the array
+        sort2(intArray);
+        // // print the entire array
+        print(intArray);
     }
 
 }
